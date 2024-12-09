d570660b0419cecc3d446f035449409284c53d95
Jkosinski11 <kosinski.jacob11@gmail.com>
2024-12-09 14:20:37 -0500

    First Regression

--- DSCIfp.bash
+++ DSCIfp.bash
@@ -1,2 +1,2 @@
-# Bash File for NHL DATA
-awk -F, '{print $3, $4, $5 , $6}' Largest-Companies.csv > company-data.csv      
+# Bash File for Company DATA
+awk -F, 'NR ==1 {printf "%s,%s,%s,%s\n", $3,$4,$5,$6} NR >1 {printf "%.2f,%.2f,%.2f,%.2f\n", $3,$4,$5,$6}' Largest-Companies.csv | sed 's/Market Value/Market_Value/' > company-data.csv      
